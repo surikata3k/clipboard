9bb8082abf1e0723ea50339b25417f904054e649
Surikata3k <surikata3k@gmail.com>
2024-12-03 16:52:32 +0100

    Update oracle_tips.sql

--- sql/oracle_tips.sql
+++ sql/oracle_tips.sql
@@ -1,3 +1,16 @@
+## Connect to sqlplus and open connection
 sql>connect / as sysdba
 sql> show con_name
+  
+## Oracle change password
 sql> ALTER USER usuari IDENTIFIED BY currentPassword REPLACE newPassword;
+
+## New oracle user
+Open SQLPlus
+´´´
+SQL> connect system/admin@127.0.0.1:1521/XEPDB1
+ 
+SQL> CREATE USER pepe IDENTIFIED BY pepe;
+ 
+SQL> grant all privileges to pepe;
+´´´
